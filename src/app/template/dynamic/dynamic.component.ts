51c756d3afd00f208472b7554ab50de0d6583fa5
jjnorris31 <jjnorris31@gmail.com>
2022-03-06 19:05:39 -0600

    feat: added dynamic component

--- src/app/template/dynamic/dynamic.component.ts
+++ src/app/template/dynamic/dynamic.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Person {
+  name: string;
+  favorites: Favorite[];
+}
+
+interface Favorite {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-dynamic',
   templateUrl: './dynamic.component.html',
@@ -7,9 +17,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicComponent implements OnInit {
 
+  person: Person = {
+    name: "Fernando",
+    favorites: [
+      {
+        id: 1, name: "Metal Gear"
+      }
+    ]
+  }
+
+  favorite: Favorite = {
+    name: "",
+    id: this.person.favorites.length + 1
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  save() {
+    console.log('posted');
+  }
+
+  addGame() {
+    this.person.favorites.push(this.favorite);
+    this.favorite = {
+      name: "",
+      id: this.person.favorites.length + 1
+    }
+  }
+
+  deleteGame(i: number) {
+    this.person.favorites = this.person.favorites.filter((item, index) => i !== index)
+  }
 }
