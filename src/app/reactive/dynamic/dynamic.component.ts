2be49003518c1316588c3c200786538789688549
jjnorris31 <jjnorris31@gmail.com>
2022-03-10 11:04:22 -0600

    feat: finished dynamic component implementation

--- src/app/reactive/dynamic/dynamic.component.ts
+++ src/app/reactive/dynamic/dynamic.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {formatNumber} from "@angular/common";
 
 @Component({
   selector: 'app-dynamic',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicComponent implements OnInit {
 
-  constructor() { }
+  dynamicForm: FormGroup = this.formBuilder.group({
+    name: ["JuanJo", [Validators.minLength(3), Validators.required]],
+    favorites: this.formBuilder.array([
+      ["Metal Gear", [Validators.required]],
+      ["Dea", [Validators.required]]], Validators.required)
+  });
+
+
+  get favoritesArray() {
+    return this.dynamicForm.get("favorites") as FormArray;
+  }
+
+  addFavorite(name: string) {
+    this.favoritesArray.push(new FormControl(name, Validators.required));
+  }
+
+  deleteFavorite(index: number) {
+    this.favoritesArray.removeAt(index);
+  }
+
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  public validField(field: string) {
+    return this.dynamicForm.controls[field].invalid;
   }
 
 }
