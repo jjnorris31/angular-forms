b6a0174ea24b9458eac692865d2fad2ea9bd61e9
jjnorris31 <jjnorris31@gmail.com>
2022-03-10 11:56:12 -0600

    feat: added switches implementation

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-switches',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  switchesForm: FormGroup = this.formBuilder.group({
+    gender: ['M', Validators.required],
+    notifications: [false, Validators.required],
+  })
+
+  person = {
+    gender: 'F',
+    notifications: true
+  }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.switchesForm.setValue(this.person);
+
+    this.switchesForm.get('gender')?.valueChanges.subscribe(value => {
+      console.log(value);
+    })
+
+    this.switchesForm.valueChanges.subscribe(form => {
+      console.log(form);
+    })
+  }
+
+  save() {
+    this.person = {...this.switchesForm.value};
   }
 
 }
